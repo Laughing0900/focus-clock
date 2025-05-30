9a32f6128ccd605d17ab6e08f202447fde6161b4
laughing <laughing0900@gmail.com>
2025-05-30 10:38:33 +0800

    Initial Commit

---
+++ focus-clock/focus_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  focus_clockApp.swift
+//  focus-clock
+//
+//  Created by Laughing Cheng on 30/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct focus_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ focus-clockTests/focus_clockTests.swift
@@ -0,0 +1,17 @@
+//
+//  focus_clockTests.swift
+//  focus-clockTests
+//
+//  Created by Laughing Cheng on 30/5/2025.
+//
+
+import Testing
+@testable import focus_clock
+
+struct focus_clockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
